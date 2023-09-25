a2ab68267c4980d10e7232d570822dd71bbb57e1
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-09-25 10:58:19 -0500

    Fix Nvim-CMP crash when using `:read!` command
    To fix the error I literally disabled the autocomplete for the `read`
    command.
    
    Additionally some key mappings were reworked, e.g. `<TAB>` is now used
    to start autocomplete.
    
    Some eye candy (rounded edges) was also added.

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -19,21 +19,20 @@ return {
 
 		cmp.setup({
 			snippet = {
-				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 				end,
 			},
 			window = {
-				-- completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = cmp.mapping.complete(),
 				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
@@ -77,32 +76,43 @@ return {
 				{ name = 'path' }
 			}, {
 				{ name = 'cmdline' }
-			})
+			}),
+			enabled = function()
+				-- Set of commands where cmp will be disabled
+				local disabled = {
+					read = true,
+				}
+				-- Get first word of cmdline
+				local cmd = vim.fn.getcmdline():match("%S+")
+				-- Return true if cmd isn't disabled
+				-- else call/return cmp.close(), which returns false
+				return not disabled[cmd] or cmp.close()
+			end,
 		})
 
 		vim.cmd [[
-" NOTE: You can use other key to expand snippet.
+		  " NOTE: You can use other key to expand snippet.
 
-" Expand
-imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
-smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+		  " Expand
+		  imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+		  smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
 
-" Expand or jump
-imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+		  " Expand or jump
+		  imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+		  smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 
-" Jump forward or backward
-imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
-imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
-smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+		  " Jump forward or backward
+		  imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+		  smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+		  imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+		  smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
 
-" Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
-" See https://github.com/hrsh7th/vim-vsnip/pull/50
-nmap        s   <Plug>(vsnip-select-text)
-xmap        s   <Plug>(vsnip-select-text)
-nmap        S   <Plug>(vsnip-cut-text)
-xmap        S   <Plug>(vsnip-cut-text)
-]]
+		  " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+		  " See https://github.com/hrsh7th/vim-vsnip/pull/50
+		  nmap        s   <Plug>(vsnip-select-text)
+		  xmap        s   <Plug>(vsnip-select-text)
+		  nmap        S   <Plug>(vsnip-cut-text)
+		  xmap        S   <Plug>(vsnip-cut-text)
+		  ]]
 	end,
 }
