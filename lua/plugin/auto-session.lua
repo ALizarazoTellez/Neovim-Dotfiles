4fa6f299b7b2a35c829c85addce1baaf9dd245c2
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-09-28 09:57:31 -0500

    Fix `Nvim-Tree` bugs with `auto-session`

--- lua/plugin/auto-session.lua
+++ lua/plugin/auto-session.lua
@@ -10,6 +10,8 @@ return {
 	opts = {
 		log_level = "error",
 		auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+		auto_session_create_enabled = false,
+		pre_save_cmds = { "tabdo NvimTreeClose" },
 
 		session_lens = {
 			load_on_setup = true,
