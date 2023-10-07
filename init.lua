a90e4b7bdf2828baabe17500c10e8875eb1caf61
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-10-06 21:20:14 -0500

    Fix lag in `fj` & `jf` shortcuts
    But on the other hand, the lag became more evident just when entering
    normal mode.

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Neovim.
+-- Neo's.
 
 -- Copyright (C) 2023, Anderson Lizarazo Tellez.
 
@@ -24,14 +25,46 @@ vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+
+function GetPrevChar()
+	local line_text = vim.api.nvim_get_current_line()
+	local cur_pos = vim.api.nvim_win_get_cursor(0)
+	if cur_pos[2] == 0 then
+		return nil
+	end
+	local prev_char = line_text:sub(cur_pos[2], cur_pos[2])
+	return prev_char
+end
+
+function BeforeIsChar(char)
+	local ch = GetPrevChar()
+	if ch == char then
+		return true
+	end
+
+	return false
+end
+
+function NonDistractingImap(prev, char, exec)
+	return function()
+		if BeforeIsChar(prev) then
+			vim.api.nvim_input(exec)
+			return
+		end
+
+		-- I can't just insert the character, otherwise this function will be called infinite times.
+		vim.api.nvim_input(" <Esc>r" .. char .. "a")
+	end
+end
+
 -- Fast alternative to <Esc> key.
-vim.keymap.set('i', 'fj', '<Esc>')
-vim.keymap.set('i', 'jf', '<Esc>')
+vim.keymap.set('i', 'f', NonDistractingImap('j', 'f', '<Esc>xh'))
+vim.keymap.set('i', 'j', NonDistractingImap('f', 'j', '<Esc>xh'))
 
 -- In some systems, the symbols `«` and `»` are very stressful of get in the
 -- keyboard, make a shortcut to generate them.
-vim.keymap.set('i', '"<', '«')
-vim.keymap.set('i', '>"', '»')
+vim.keymap.set('i', '<', NonDistractingImap('"', '<lt>', '<Esc>r«a'))
+vim.keymap.set('i', '"', NonDistractingImap('>', '"', '<Esc>r»a'))
 
 -- Auto move cursor when the backtick is pressed.
 vim.keymap.set('i', '``', '``<Esc>i')
