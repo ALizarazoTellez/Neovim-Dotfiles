c3e5594182ba6015b8f459b302703b8d23069bce
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-10-10 13:13:52 -0500

    Use native method of mapping in Insert Mode
    In the end I think I will have to get used to seeing the lag, since the
    previous method gave more problems than solutions.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Neovim.
--- Neo's.
+--
 
 -- Copyright (C) 2023, Anderson Lizarazo Tellez.
 
@@ -21,50 +21,18 @@ vim.o.wrap = false
 vim.o.undofile = true
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
--- Disable NetRW (Replaced by plugin NvimTree).
+-- Disable NetRW (replaced by plugin NvimTree).
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-
-function GetPrevChar()
-	local line_text = vim.api.nvim_get_current_line()
-	local cur_pos = vim.api.nvim_win_get_cursor(0)
-	if cur_pos[2] == 0 then
-		return nil
-	end
-	local prev_char = line_text:sub(cur_pos[2], cur_pos[2])
-	return prev_char
-end
-
-function BeforeIsChar(char)
-	local ch = GetPrevChar()
-	if ch == char then
-		return true
-	end
-
-	return false
-end
-
-function NonDistractingImap(prev, char, exec)
-	return function()
-		if BeforeIsChar(prev) then
-			vim.api.nvim_input(exec)
-			return
-		end
-
-		-- I can't just insert the character, otherwise this function will be called infinite times.
-		vim.api.nvim_input(" <Esc>r" .. char .. "a")
-	end
-end
-
 -- Fast alternative to <Esc> key.
-vim.keymap.set('i', 'f', NonDistractingImap('j', 'f', '<Esc>xh'))
-vim.keymap.set('i', 'j', NonDistractingImap('f', 'j', '<Esc>xh'))
+vim.keymap.set('i', 'fj', '<Esc>')
+vim.keymap.set('i', 'jf', '<Esc>')
 
 -- In some systems, the symbols `«` and `»` are very stressful of get in the
 -- keyboard, make a shortcut to generate them.
-vim.keymap.set('i', '<', NonDistractingImap('"', '<lt>', '<Esc>r«a'))
-vim.keymap.set('i', '"', NonDistractingImap('>', '"', '<Esc>r»a'))
+vim.keymap.set('i', '"<', '«')
+vim.keymap.set('i', '>"', '»')
 
 -- Auto move cursor when the backtick is pressed.
 vim.keymap.set('i', '``', '``<Esc>i')
