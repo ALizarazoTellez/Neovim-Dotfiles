63d599a83e64361d89680966cf23db338341b972
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-11-08 15:56:37 -0500

    Adjust autocomplete triggers

--- lua/plugin/nvim-cmp.lua
+++ lua/plugin/nvim-cmp.lua
@@ -13,6 +13,8 @@ return {
 		local snippy = require('snippy')
 
 		cmp.setup({
+			enabled = true,
+
 			--[[
 			  Completion mode:
 				1. If the autocomplete window is not present, the <Tab> key will launch it, but will not choose any options.
@@ -24,6 +26,22 @@ return {
 				['<S-Tab>'] = cmp.mapping.select_prev_item(),
 				['<Enter>'] = cmp.mapping.confirm({ select = false }),
 			},
+
+			completion = {
+				-- It must be a very precise suggestion to launch the autocomplete.
+				keyword_length = 3,
+			},
+
+			formatting = {
+				expandable_indicator = true,
+			},
+
+			performance = {
+				-- Many options are more distracting than helpful.
+				max_view_entries = 14,
+			},
+
+			preselect = cmp.PreselectMode.Item,
 
 			-- This function is used to insert text to the buffer, even if it is not a snippet, do not change it unless you know what you are doing.
 			snippet = {
