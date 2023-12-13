4c24d056feb7644534e28a0202356b27b8e742ea
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2023-12-13 09:05:21 -0500

    Add keymap for `NoNeckPain` command

--- lua/plugin/no-neck-pain.lua
+++ lua/plugin/no-neck-pain.lua
@@ -2,6 +2,10 @@ return {
 	"shortcuts/no-neck-pain.nvim",
 	version = "*",
 
+	keys = {
+		{ '<Leader>np', '<Cmd>NoNeckPain<Enter>', desc = 'Toggle NoNeckPain plugin.' },
+	},
+
 	opts = {
 		buffers = {
 			wo = {
